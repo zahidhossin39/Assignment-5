3cf2e67123c0674a80fe3127246d4b5781710e85
zahidhossin39 <zahidhossin39@gmail.com>
2026-03-09 17:12:32 +0600

    Made the login section fully functional with javaScript

---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+document.getElementById("login-btn").addEventListener("click",()=>{
+    // usernameInput
+    const usernameInput=document.getElementById("input-username")
+    const userName = usernameInput.value;
+    console.log(userName);
+
+    // PasswordInput 
+    const PasswordInput=document.getElementById("input-password")
+    const password =PasswordInput.value;
+    console.log(password);
+
+    if (userName =="admin" && password =="admin123"){
+        alert("login Success")
+
+        window.location.assign("/home.html")
+    }else{
+        alert("Login Failed");
+        return;
+    }
+})
+
